4786135f9901cfab6a767eff1c41d7be4c6b16d6
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-01 14:25:28 +0200

    [UPD] added method for data collection and validation

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// Primo sotto problema: Raccolta e validazione dei dati
+let km = parseFloat(prompt("Inserisci i km da percorrere"));
+let age = parseInt(prompt("Inserisci la tua età"));
+//Calcolo per il prezzo base del biglietto
+let price = km * 0.21;
