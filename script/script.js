d28a4b14be75adb04944da3a7fd72f010840aa56
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-01 14:37:57 +0200

    [UPD]  added a price calculation taking into account discounts

--- script/script.js
+++ script/script.js
@@ -3,3 +3,10 @@ let km = parseFloat(prompt("Inserisci i km da percorrere"));
 let age = parseInt(prompt("Inserisci la tua età"));
 //Calcolo per il prezzo base del biglietto
 let price = km * 0.21;
+
+// Secondo sotto problema: Calcolo del prezzo scontato
+if (age < 18) {
+  price = price * 0.8; // Sconto del 20%
+} else if (age >= 65) {
+  price = price * 0.6; // Sconto del 40%
+}
